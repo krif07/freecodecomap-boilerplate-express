b00c5aeac4a2b88f1ed156e4be7aea95aaa1fe4b
cfdavila <cfdavila@indracompany.com>
2022-12-18 14:41:01 -0500

    get method, respond.sendFile

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
+
+app.get('/', function(req, res){
+    let absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+});
+
 app.get('/', function(req, res){
     res.send("Hello Express");
 });

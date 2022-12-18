c5c6fba17b909fd59b2d03149237923087587715
cfdavila <cfdavila@indracompany.com>
2022-12-18 18:58:16 -0500

    .env file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
@@ -5,6 +6,9 @@ console.log("Hello World");
 
 app.get('/json', function(req, res){
     const obj = {"message": "Hello json"};
+    if(process.env.MESSAGE_STYLE==='uppercase'){
+        obj.message = obj.message.toUpperCase();
+    }
     res.json(obj);
 });
 app.get('/', function(req, res){

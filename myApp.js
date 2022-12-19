b3f6c3adba9158f03f0a5b2fde65e7a5b289b066
Cristian Fernando Dávila <cristian.fernando.c.davila.lopez.-nd@disney.com>
2022-12-19 18:35:00 -0500

    app.use middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,12 @@ let app = express();
 
 console.log("Hello World");
 
+app.use(function middleware(req, req, next){
+    const info = req.method + ' ' + req.path + ' - ' + req.ip;
+    console.log(info)
+    next();
+});
+
 app.get('/json', function(req, res){
     const obj = {"message": "Hello json"};
     if(process.env.MESSAGE_STYLE==='uppercase'){
